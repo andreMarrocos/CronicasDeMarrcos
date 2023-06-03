756024761682084fc553f9939ab6659dc3fecbe5
andreMarrocos <107271658+andreMarrocos@users.noreply.github.com>
2023-06-03 00:31:38 -0300

    Items add and fully working
    -initial items add
    -health bar class add
    -early coin system add

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 fps = 60
+TILE_SIZE = 50 ##redefinir quando fizer o level
 
 #define player action variables
 moving_left = False
@@ -26,13 +27,23 @@ hearth_img = pygame.image.load('Images/items/hearth.png').convert_alpha()
 coin_img = pygame.image.load('Images/items/coin.png').convert_alpha()
 shield_img = pygame.image.load('Images/items/shield.png').convert_alpha()
 coin_img = pygame.transform.scale_by(coin_img, 2)
+hearth_img = pygame.transform.scale_by(hearth_img, 0.03)
+shield_img = pygame.transform.scale_by(shield_img, 0.1)
+item_boxes = {'Health': hearth_img, 'Shield': shield_img, 'Coin': coin_img}
 
 # Colors
 black = (0, 0, 0)
 green = (0, 255, 0)
 red = (255, 0, 0)
 gray = (128, 128, 128)
+blue = (30, 144, 255)
 
+#define font
+font = pygame.font.SysFont('Futura', 30)
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed, health):
@@ -44,6 +55,7 @@ class Player(pygame.sprite.Sprite):
         self.max_health = self.health
         self.speed = speed
         self.direction = 1
+        self.wallet = 0
         self.flip = False
         self.animation_list = []
         self.frame_index = 0
@@ -153,10 +165,47 @@ class Player(pygame.sprite.Sprite):
 
     def draw(self):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+        pygame.draw.rect(screen, red, self.rect, 1)
 
 class ItemBox(pygame.sprite.Sprite):
-    def __init__(self, x, y, target_x, target_y):
+    def __init__(self, item_type, x, y):
         pygame.sprite.Sprite.__init__(self)
+        self.item_type = item_type
+        self.image = item_boxes[self.item_type]
+        self.rect = self.image.get_rect()
+        self.rect.midtop = (x, y)
+
+    def update(self):
+        if pygame.sprite.collide_rect(self, player):
+            if self.item_type == "Health":
+                player.health += 25
+                if player.health > player.max_health:
+                    player.health = player.max_health
+            elif self.item_type == "Shield":
+                player.health += 50
+            elif self.item_type == "Coin":
+                player.wallet += 5
+            self.kill()
+
+
+class HealthBar():
+    def __init__(self, x, y, health, max_health):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.max_health = max_health
+
+    def draw(self, health):
+        # update with new health
+        self.health = health
+        ratio = self.health / self.max_health
+        pygame.draw.rect(screen, black, (self.x-2, self.y-2, 154, 24))
+        pygame.draw.rect(screen, red, (self.x, self.y, 150, 20))
+        pygame.draw.rect(screen, green, (self.x, self.y, 150 * ratio, 20))
+        if self.health > self.max_health:
+            pygame.draw.rect(screen, black, (self.x + 152, self.y - 2, (150 * ratio)-150, 24))
+            pygame.draw.rect(screen, blue, (self.x + 150, self.y, (150 * ratio)-150, 20))
+
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, target_x, target_y):
@@ -202,7 +251,7 @@ class Bullet(pygame.sprite.Sprite):
         if pygame.sprite.spritecollide(enemy, bullet_group, False):
             if enemy.alive:
                 enemy.health -= 20
-                print(enemy.health)
+                print(f'Vida do imigo: {enemy.health}')
                 self.kill()
 
 
@@ -210,16 +259,28 @@ class Bullet(pygame.sprite.Sprite):
 
 #create sprite groups
 bullet_group = pygame.sprite.Group()
+item_box_group = pygame.sprite.Group()
 
 
+#temp - testing items
+item_box = ItemBox('Coin', 100, 300)
+item_box_group.add(item_box)
+item_box = ItemBox('Shield', 400, 500)
+item_box_group.add(item_box)
+item_box = ItemBox('Health', 200, 500)
+item_box_group.add(item_box)
+item_box = ItemBox('Coin', 300, 300)
+item_box_group.add(item_box)
 
 player = Player('player', 200, 200, 1, 3, 100)
+health_bar = HealthBar(85, 13, player.health, player.health)
+
 enemy = Player('enemy', 400, 200, 1, 3, 60)
 
 
 
 def play():
-    global moving_right, moving_left, moving_up, moving_down,shoot
+    global moving_right, moving_left, moving_up, moving_down, shoot
     pygame.display.set_caption('Crônicas de Marrcos')
     can_shoot = True
 
@@ -279,8 +340,17 @@ def play():
         player.move(moving_left, moving_up, moving_right, moving_down)
         # update and draw groups
         screen.fill(gray)
+
+        # show player status
+        draw_text(f"Coins:{player.wallet}", font, black, 10, 35)
+        draw_text("Health:", font, black, 10, 15)
+        for x in range(player.health):
+            screen.blit
+        health_bar.draw(player.health)
         bullet_group.update()
         bullet_group.draw(screen)
+        item_box_group.update()
+        item_box_group.draw(screen)
         player.update()
         player.draw()
         enemy.update()
